4105a881ba3bcb6ba0e659c1200266a880b6dd0f
Son Dinh <technology789@gmail.com>
2019-01-31 15:41:33 -0600

    fixed

--- 12_5_UnknownLength.cpp
+++ 12_5_UnknownLength.cpp
@@ -5,28 +5,36 @@ using namespace std;
 
 // Search a sorted array with unknown length.
 int search(int target, const vector<int>& arr ) {
-	int left = 0;
-	int right = 1000; // some random upper bound
-	int mid = (left + right)/2;
-	while (left <= right) {
+	int p = 0;
+	while (true) {
+		int i = (1 << p) - 1;
 		try {
-			// simulate that the length of the array is not known.
-			int size = arr.size();
-			if (mid >= size) throw mid;
+			if (arr.at(i) == target) return i;
+			if (arr.at(i) > target) break;
+		}
+		catch(const exception& e) {
+			break;
+		}
+		p++;
+	}
 
-			if (arr[mid] == target) {
+	int left = p == 0? 0 : 1 << (p - 1);
+	int right = p == 0? 0 : (1 << p) - 2;
+	while (left <= right) {
+		int mid = left + (right - left)/2;
+		try {
+			if (arr.at(mid) == target) {
 				return mid;
-			} else if (arr[mid] < target) {
+			} else if (arr.at(mid) < target) {
 				left = mid + 1;
 			} else {
 				right = mid - 1;
 			}
-			mid = (left + right)/2;
-		} catch(int x) {
-			right = x - 1;
-			mid = (left + right)/2;
+		} catch(const exception& ex) {
+			right = mid - 1;
 		}
 	}
+
 	return -1;
 }
 
@@ -35,5 +43,13 @@ int main() {
 	cout << "Index of 11: " << search(11, arr) << endl;
 	cout << "Index of 100: " << search(100, arr) << endl;
 	cout << "Index of -7: " << search(-7, arr) << endl;
+
+	vector<int> arr2;
+	for (int i = 0; i < 200; i++) {
+		arr2.push_back(i);
+	}
+	cout << "Index of 150: " << search(150, arr2) << endl;
+
+	cout << "Index of 10 in an empty array: " << search(10, vector<int>()) << endl;
 	return 0;
 }
