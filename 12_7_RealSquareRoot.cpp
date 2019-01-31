50d1c23b701421e422d6e3b1a889d740d6ffefaf
Son Dinh <technology789@gmail.com>
2019-01-31 17:38:03 -0600

    square root of real number

---
+++ 12_7_RealSquareRoot.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+enum Order {SMALLER, EQUAL, GREATER};
+
+Order compare(double a, double b) {
+	double diff = a - b;
+	if (diff < -numeric_limits<double>::epsilon())
+		return SMALLER;
+	else if (diff > numeric_limits<double>::epsilon())
+		return GREATER;
+	else
+		return EQUAL;
+}
+
+double root(double x) {
+	double left = 0;
+	double right = x;
+	
+	while (compare(left, right) == SMALLER) {
+		double mid = (left + right)/2;
+		double square = mid * mid;
+		if (compare(square, x) == EQUAL) {
+			return mid;
+		} else if (compare(square, x) == GREATER) {
+			right = mid - numeric_limits<double>::epsilon();
+		} else {
+			left = mid + numeric_limits<double>::epsilon();
+		}
+	}
+	return right;
+}
+
+int main() {
+	double x;
+	cin >> x;
+	cout << "Square root of " << x << " : " << root(x) << endl;
+	return 0;
+}
