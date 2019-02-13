ac6e96406fb93e8a6c7690ffbf6bb8edc99a90bf
Son Dinh <technology789@gmail.com>
2019-02-12 23:19:40 -0600

    merge sorted files

--- 10_6_RootToLeavesSum.cpp
+++ 10_6_RootToLeavesSum.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 // Each node contains a binary digit.
+// @node: current node being traversed.
+// @total: reference to the total sum of root-to-leaf paths computed so far.
+// @curr: value for the root-to-leaf for the current path.
 void helper(const unique_ptr<BinaryTreeNode<int>>& node, int& total, int curr) {
 	if (node == nullptr)
 		return;
@@ -44,8 +47,8 @@ int main() {
 	root->left->right->data = 1;
 	root->right->left = unique_ptr<BinaryTreeNode<int>> (new struct BinaryTreeNode<int>);
 	root->right->left->data = 0;
-	root->right->right = unique_ptr<BinaryTreeNode<int>> (new struct BinaryTreeNode<int>);
-	root->right->right->data = 0;
+	//	root->right->right = unique_ptr<BinaryTreeNode<int>> (new struct BinaryTreeNode<int>);
+	//	root->right->right->data = 0;
 
 	cout << "Sum of root-to-leaf paths: " << sum(root) << endl;
 	
---
+++ 11_1_MergeSortedFiles.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm
+
+using namespace std;
+
+void merge(vector<int>& infs, vector<int>& of) {
+	
+}
+
+int main() {
+	// Simulate 500 files, each contains a list of increasing trade times.
+	vector<int> infiles[500];
+	vector<int> outfile;
+	merge(infiles, outfile);
+	
+	return 0;
+}
