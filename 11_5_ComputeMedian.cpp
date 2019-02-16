2511a4ab7dee8a09192b0a2069b1e3f4bddb7ec7
Son Dinh <technology789@gmail.com>
2019-02-16 14:11:16 -0600

    use input stream

--- 11_5_ComputeMedian.cpp
+++ 11_5_ComputeMedian.cpp
@@ -5,16 +5,16 @@
 using namespace std;
 
 // Compute the medians of an online stream of numbers.
-vector<int> median(const vector<int>& sequence) {
+void median() {
 	priority_queue<int, vector<int>, greater<int>> min_heap;
 	priority_queue<int, vector<int>, less<int>> max_heap;
 
-	vector<int> ret;
-	for (int i = 0; i < sequence.size(); i++) {
-		if (!min_heap.empty() && sequence[i] < min_heap.top()) {
-			max_heap.push(sequence[i]);
+	int next_num;
+	while (cin >> next_num) {
+		if (!min_heap.empty() && next_num < min_heap.top()) {
+			max_heap.emplace(next_num);
 		} else {
-			min_heap.push(sequence[i]);
+			min_heap.emplace(next_num);
 		}
 
 		if (min_heap.size() > max_heap.size() + 1) {
@@ -27,20 +27,38 @@ vector<int> median(const vector<int>& sequence) {
 			max_heap.pop();
 		}
 
-		ret.push_back(min_heap.size() == max_heap.size()? (min_heap.top()+max_heap.top())/2 : (min_heap.size() > max_heap.size()? min_heap.top() : max_heap.top()) );
+		cout << "Median: " << (min_heap.size() == max_heap.size()? 0.5*(min_heap.top()+max_heap.top()) :
+							   (min_heap.size() > max_heap.size()? min_heap.top() : max_heap.top())) << endl;
 	}
-	return ret;
+}
+
+// The solution from the book is incorrect. Try sequence: 100 200 50.
+// The last median returned is 50, which is wrong because it must be 100.
+// The reason is that the code does not guarantee an invariant that
+// the top number of the min-heap must be greater-than-or-equal-to the
+// top number of the max-heap. 
+void epi_sol() {
+	priority_queue<int, vector<int>, greater<int>> min_heap;
+	priority_queue<int, vector<int>, less<int>> max_heap;
+
+	cin.clear();
+	int x;
+	while (cin >> x) {
+		min_heap.emplace(x);
+		if (min_heap.size() > max_heap.size() + 1) {
+			max_heap.emplace(min_heap.top());
+			min_heap.pop();
+		}
+
+		cout << "Median: " << (min_heap.size() == max_heap.size() ?
+							   0.5*(min_heap.top() + max_heap.top()) : min_heap.top()) << endl;
+	}
 }
 
 int main() {
-	vector<int> sequence{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	vector<int> ret = median(sequence);
-
-	cout << "Running median: ";
-	for (auto med : ret) {
-		cout << med << " ";
-	}
-	cout << endl;
-
+	median();
+	cout << "EPI Solution:" << endl;
+	epi_sol();
+	
 	return 0;
 }
