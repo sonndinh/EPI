54e2de6ce432fbd90015d3922dc86cb9ef7539e3
Son Dinh <technology789@gmail.com>
2019-02-24 23:55:19 -0600

    fixed

--- 13_2_Palindromic.cpp
+++ 13_2_Palindromic.cpp
@@ -1,38 +1,27 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
-// Test if a string can be permuted to form a palindrome.
-// O(nlgn) with n is the length of string.
+// O(n) time. O(m) space with m is the number of distinct characters.
 bool palindromic(string s) {
-	sort(s.begin(), s.end());
-	bool has_single = false;
-	int count = 1;
-	for (int i = 1; i < s.size(); i++) {
-		if (s[i-1] == s[i]) {
-			count++;
-			if (count > 2) return false;
-		} else {
-			if (count == 1 && has_single == false)
-				has_single = true;
-			else if (count == 2)
-				count = 1;
-			else
-				return false;
-		}
+	unordered_map<char, int> dict;
+	for (int i = 0; i < s.size(); i++) {
+		dict[s[i]]++;
 	}
-	if (count == 1 && has_single == true) return false;
-	return true;
-}
 
-// O(n).
-bool palindromic2(string s) {
+	int count_odds = 0;
+	for (const pair<char, int>& p : dict) {
+		if (p.second % 2 == 1)
+			count_odds++;
+	}
+	return count_odds == 0 || count_odds == 1;
 }
 
 int main() {
-	string s = "edified";
-	cout << "String \"edified\" is " << (palindromic(s)? "palindromic" : "not palindromic") << endl;
+	string s = "ediffied";
+	cout << "String \"ediffied\" is " << (palindromic(s)? "palindromic" : "NOT palindromic") << endl;
 	return 0;
 }
