f2a8f797b6b3ffa747ef78442c63efda3fafeb2f
Son Dinh <technology789@gmail.com>
2019-03-07 21:49:19 -0600

    no temporary stack

--- 9_9_SortStack.cpp
+++ 9_9_SortStack.cpp
@@ -2,6 +2,7 @@
 #include <stack>
 using namespace std;
 
+// Sort a stack using a temporary stack.
 void sort_stack(stack<int>& s) {
     stack<int> tmp;
     while (!s.empty()) {
@@ -26,6 +27,27 @@ void sort_stack(stack<int>& s) {
     }
 }
 
+// Insert t to an already sorted sub-stack.
+void helper(stack<int>& s, int t) {
+    if (s.empty() || s.top() <= t) {
+        s.push(t);
+    } else {
+        int x = s.top();
+        s.pop();
+        helper(s, t);
+        s.push(x);
+    }
+}
+
+// Sort a stack without explicitly using another data structure.
+void sort_stack2(stack<int>& s) {
+    if (s.empty()) return;
+    int t = s.top();
+    s.pop();
+    sort_stack2(s);
+    helper(s, t);
+}
+
 // To execute C++, please define "int main()"
 int main() {
     stack<int> s;
@@ -33,7 +55,7 @@ int main() {
         s.push(x);
     }
 
-    sort_stack(s);
+    sort_stack2(s);
     cout << "Output:" << endl;
     while (!s.empty()) {
         int x = s.top();
