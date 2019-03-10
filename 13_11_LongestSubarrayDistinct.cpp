8e7b8daf41cb067cf070684cc7d5f287638f38c0
Son Dinh <technology789@gmail.com>
2019-03-09 22:37:40 -0600

    update

--- 13_11_LongestSubarrayDistinct.cpp
+++ 13_11_LongestSubarrayDistinct.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 // Return the longest subarray with distinct entries. 
-pair<int, int> subarray(const vector<char>& A) {
+pair<int, int> subarray2(const vector<char>& A) {
 	if (A.size() == 0) return {0, 0};
 
 	// Maintain a list of distinct entries in the current subarray.
@@ -35,6 +35,38 @@ pair<int, int> subarray(const vector<char>& A) {
 	}
 
 	return {max_start, max_len};
+}
+
+pair<int, int> subarray(const vector<char>& A) {
+	if (A.size() == 0) return {0, 0};
+
+	unordered_map<char, int> recent_occur;
+	int max_start = 0, max_end = 0;
+	int curr_start = 0, curr_end = 0;
+	for (int i = 0; i < A.size(); i++) {
+		char c = A[i];
+		// Proactively emplace this to the map.
+		auto old = recent_occur.emplace(c, i);
+		// If the same entry exists in the map, check if it is
+		// in the current subarray. If it is, we update the maximum length. 
+		if (!old.second) {
+			int old_idx = old.first->second; // Previous occurrence of the entry.
+			if (old_idx >= curr_start) {
+				if (i - 1 - curr_start > max_end - max_start) {
+					max_start = curr_start;
+					max_end = i - 1;
+				}
+				curr_start = old_idx + 1;
+			}
+			recent_occur[c] = i;
+		}
+	}
+
+	if (A.size() - curr_start > max_end - max_start + 1) {
+		max_start = curr_start;
+		max_end  = A.size() - 1;
+	}
+	return {max_start, max_end-max_start+1};
 }
 
 int main() {
