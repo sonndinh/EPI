0850415203c60e85610481e7b139eed93e7327ed
Son Dinh <technology789@gmail.com>
2019-03-14 20:42:45 -0500

    update

--- others/RegexMatching.cpp
+++ others/RegexMatching.cpp
@@ -5,58 +5,63 @@ using namespace std;
 // Return whether a string matches a pattern.
 // A '.' matches any single character. A '*' matches zero or more
 // preceding character in the pattern. 
-bool match(string txt, string pat) {
+bool is_match(string txt, string pat) {
 	int n = txt.size();
-	int m = 0; // '*' is grouped with its preceding character.
-	for (int i = pat.size()-1; i >= 0; i--) {
-		if (pat[i] == '*') {
-			i--;
-		}
-		m++;
-	}
-
-	vector<vector<bool>> table(n+1, vector<bool>(m+1));
+	int m = pat.size();
+	vector<vector<bool>> dp(n+1, vector<bool>(m+1));
+	
 	for (int i = 0; i <= n; i++) {
-		// Initialize values for empty pattern.
-		table[i][0] = true;
-	}
-	int idx = 0;
-	for (int j = 1; j <= m; j++) {
-		if (j == 1 && pat[j] == '*') {
-			table[0][j] = true;
-			idx += 2;
-		} else if (pat[idx+1] == '*'){
-			table[0][j] = table[0][j-1];
-			idx += 2;
-		} else {
-			table[0][j] = false;
-			idx += 1;
-		}
-	}
-
-	for (int i = 1; i <= n; i++) {
-		int idx = 0; // Index of character in the pattern.
-		for (int j = 1; j <= m; j++) {
-			if (idx <= pat.size()-2 && pat[idx+1] == '*') {
-				if (pat[idx] != txt[i-1]) {
-					table[i][j] = false;
-					idx += 2;
-				} else
-					table[i][j] = true;
-			} else if (pat[idx] == '.' || pat[idx] == txt[i-1]) {
-				// Character match.
-				table[i][j] = table[i-1][j-1];
-				idx++;
+		for (int j = 0; j <= m; j++) {
+			if (j == 0) {
+				// Empty pattern.
+				dp[i][j] = true;
+			} else if (i == 0) {
+				// Empty text, non-empty pattern.
+				if (j < m && pat[j] == '*') {
+					if (j > 1 && pat[j-2] != '*')
+						dp[i][j] = dp[i][j-1];
+					else if (j > 1 && pat[j-2] == '*')
+						dp[i][j] = dp[i][j-2];
+					else
+						dp[i][j] = true;
+				} else {
+					dp[i][j] = false;
+				}
 			} else {
-				// Not match.
-				table[i][j] = false;
-				idx++;
+				bool first_match = pat[j-1] == '.' || pat[j-1] == txt[i-1];
+				if (j < m && pat[j] == '*') {
+					if (first_match && j > 1 && pat[j-2] == '*') {
+						dp[i][j] = dp[i][j-2] || dp[i-1][j];
+					} else if (first_match && j > 1 && pat[j-2] != '*') {
+						dp[i][j] = dp[i][j-1] || dp[i-1][j];
+					} else if (!first_match && j > 1 && pat[j-2] == '*') {
+						dp[i][j] = dp[i][j-2];
+					} else {
+						dp[i][j] = dp[i][j-1];
+					}
+				} else if (j < m && pat[j] != '*'){
+					if (j > 1 && pat[j-2] == '*') {
+						dp[i][j] = first_match && dp[i-1][j-2];
+					} else {
+						dp[i][j] = first_match && dp[i-1][j-1];
+					}
+				} else {
+					if (j > 1 && pat[j-2] == '*') {
+						dp[i][j] = first_match && dp[i-1][j-2];
+					} else {
+						dp[i][j] = first_match && dp[i-1][j-1];
+					}
+				}
 			}
 		}
 	}
-	return table[n][m];
+	return dp[n][m];
 }
 
 int main() {
+	string text = "a";
+	string pattern = "a*";
+	bool ret = is_match(text, pattern);
+	cout << "String: " << text << " and pattern: " << pattern << (ret? " match!" : " not match!") << endl;
 	return 0;
 }
