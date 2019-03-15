0e7968a271d038f68a7b0d33a046b566dd0539bb
Son Dinh <technology789@gmail.com>
2019-03-14 21:44:48 -0500

    bottom up

--- others/RegexMatching.cpp
+++ others/RegexMatching.cpp
@@ -8,6 +8,7 @@ using namespace std;
 bool is_match(string txt, string pat) {
 	int n = txt.size();
 	int m = pat.size();
+	// Entry (i, j) in the table indicates whether text[0:i] matches pattern[0:j].
 	vector<vector<bool>> dp(n+1, vector<bool>(m+1));
 	
 	for (int i = 0; i <= n; i++) {
@@ -55,13 +56,38 @@ bool is_match(string txt, string pat) {
 			}
 		}
 	}
+	
 	return dp[n][m];
 }
 
+bool is_match2(string txt, string pat) {
+	int n = txt.size();
+	int m = pat.size();
+	// Entry (i, j) in the table indicates whether text[i:] matches pattern[j:].
+	vector<vector<bool>> dp(n+1, vector<bool>(m+1));
+	dp[n][m] = true;
+
+	for (int i = n; i >= 0; i--) {
+		for (int j = m-1; j >= 0; j--) {
+			bool first_match = i < n && (pat[j] == '.' || txt[i] == pat[j]);
+			if (j < m - 1 && pat[j+1] == '*') {
+				if (!first_match)
+					dp[i][j] = dp[i][j+2];
+				else
+					dp[i][j] = dp[i][j+2] || dp[i+1][j];
+			} else {
+				dp[i][j] = first_match && dp[i+1][j+1];
+			}
+		}
+	}
+
+	return dp[0][0];
+}
+
 int main() {
-	string text = "a";
-	string pattern = "a*";
-	bool ret = is_match(text, pattern);
-	cout << "String: " << text << " and pattern: " << pattern << (ret? " match!" : " not match!") << endl;
+	string text = "abbcc";
+	string pattern = "a*b*c.";
+	bool ret = is_match2(text, pattern);
+	cout << "String: \"" << text << "\" and pattern: \"" << pattern << (ret? "\" match!" : "\" not match!") << endl;
 	return 0;
 }
