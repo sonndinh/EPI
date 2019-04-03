847809694dc3257074873925882c3dde80e238a6
Son Dinh <technology789@gmail.com>
2019-04-02 19:41:35 -0500

    add EPI solution

--- 18_13_LongestSubarray.cpp
+++ 18_13_LongestSubarray.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 using namespace std;
 
 // Return a longest subarray with sum less than or equal to k.
@@ -29,10 +30,45 @@ pair<int, int> subarray(const vector<int>& A, int k) {
 	return {start, end};
 }
 
+// EPI solution. Time complexity O(n), space complexity O(n).
+pair<int, int> longest_subarray(const vector<int>& A, int k) {
+	vector<int> prefix_sum;
+	partial_sum(A.cbegin(), A.cend(), back_inserter(prefix_sum));
+
+	if (prefix_sum.back() <= k) {
+		return {0, A.size()-1};
+	}
+
+	vector<int> min_prefix_sum(A.size());
+	min_prefix_sum.back() = prefix_sum.back();
+	for(int i = min_prefix_sum.size()-2; i >= 0; i--) {
+		min_prefix_sum[i] = min(prefix_sum[i], min_prefix_sum[i+1]);
+	}
+
+	int a = 0, b = 0, max_length = 0;
+	pair<int, int> ret(-1, -1);
+	while (a < A.size() && b < A.size()) {
+		int min_curr_sum = a > 0 ? min_prefix_sum[b] - prefix_sum[a-1] : min_prefix_sum[b];
+		if (min_curr_sum <= k) {
+			int curr_length = b - a + 1;
+			if (curr_length > max_length) {
+				max_length = curr_length;
+				ret = {a, b};
+			}
+			++b;
+		} else {
+			++a;
+		}
+	}
+
+	return ret;
+}
+
+
 int main() {
 	vector<int> A{431, -15, 639, 342, -14, 565, -924, 635, 167, -70};
 	int k = 184;
-	pair<int, int> p = subarray(A, k);
+	pair<int, int> p = longest_subarray(A, k);
 	cout << "Longest subarray with sum <= " << k << ": " << p.first << ", " << p.second << endl;
 	return 0;
 }
