439edd1da6c8e8d0e55aca1c06061393eb641709
Son Dinh <technology789@gmail.com>
2019-04-03 22:05:30 -0500

    wildcard matching

---
+++ others/WildcardMatching.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Implement wildcard pattern matching.
+// '?' matches any single character.
+// '*' matches any sequence of characters, including empty.
+// E.g., ("aab", "c*a*b") -> 0.
+int is_match(const string& s, const string& p) {
+	
+}
+
+int main() {
+	return 0;
+}
