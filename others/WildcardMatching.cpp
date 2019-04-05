cd297d503db2cff9e841940a9773cb34679a37b9
Son Dinh <technology789@gmail.com>
2019-04-05 00:15:53 -0500

    recursive

--- others/WildcardMatching.cpp
+++ others/WildcardMatching.cpp
@@ -5,11 +5,21 @@ using namespace std;
 
 // Recursive implementation.
 bool is_match_helper(const string& s, int i, const string& p, int j) {
+	// Some characters in the text do not match.
 	if (j >= p.size() && i < s.size())
 		return false;
+
+	// Some characters left in the pattern.
 	if (j < p.size() && i >= s.size()) {
-		
+		if (p[j] == '*')
+			return is_match_helper(s, i, p, j+1);
+		else
+			return false;
 	}
+
+	// All characters match.
+	if (j >= p.size() && i >= s.size())
+		return true;
 		
 	if (p[j] == '*') {
 		return is_match_helper(s, i, p, j+1) || is_match_helper(s, i+1, p, j+1)
@@ -19,6 +29,8 @@ bool is_match_helper(const string& s, int i, const string& p, int j) {
 	if (p[j] == '?' || p[j] == s[i]) {
 		return is_match_helper(s, i+1, p, j+1);
 	}
+	
+	return false;
 }
 
 // Implement wildcard pattern matching.
@@ -30,6 +42,12 @@ int is_match(const string& s, const string& p) {
 }
 
 int main() {
-	is_match("aab", "c*a*b");
+	string s = "aa", s2 = "aab", s3 = "ab", s4 = "bb", s5 = "abcd";
+	string p = "aa*", p2 = "aaa", p3 = "?*", p4 = "*", p5 = "*??**";
+	cout << "String: " << s << ", pattern: " << p << ": " << (is_match(s, p)? "Match" : "Not match") << endl;
+	cout << "String: " << s2 << ", pattern: " << p2 << ": " << (is_match(s2, p2)? "Match" : "Not match") << endl;
+	cout << "String: " << s3 << ", pattern: " << p3 << ": " << (is_match(s3, p3)? "Match" : "Not match") << endl;
+	cout << "String: " << s4 << ", pattern: " << p4 << ": " << (is_match(s4, p4)? "Match" : "Not match") << endl;
+	cout << "String: " << s5 << ", pattern: " << p5 << ": " << (is_match(s5, p5)? "Match" : "Not match") << endl;
 	return 0;
 }
