0db0d3748b9074698da14d74eccb57e7a470377a
Son Dinh <technology789@gmail.com>
2019-04-10 12:07:21 -0500

    allow duplicates

--- others/SmallestSubstringContainsAllCharacters.cpp
+++ others/SmallestSubstringContainsAllCharacters.cpp
@@ -5,16 +5,30 @@
 #include <unordered_map>
 using namespace std;
 
+// Return true if c_map is a subset of count.
+bool is_satisfied(const unordered_map<char, int>& c_map, const unordered_map<char, int>& count) {
+	for (const pair<char, int>& p : c_map) {
+		if (count.find(p.first) == count.end() || count.at(p.first) < p.second) {
+			return false;
+		}
+	}
+	return true;
+}
+
 // Find the smallest substring of the given string that
 // contains all characters in a given set of characters.
+// List of input characters can contain duplicates.
 string smallest_substr(string& s, vector<char>& c) {
-	// Hash set of the characters.
-	unordered_set<char> c_set(c.begin(), c.end());
-
+	// Keep the count for each input character.
+	unordered_map<char, int> c_map;
+	for (char ch : c) {
+		c_map[ch]++;
+	}
+	
 	// Store the appearances of the characters in the input string.
 	vector<pair<int, char>> pos;
 	for (int i = 0; i < s.size(); i++) {
-		if (c_set.find(s[i]) != c_set.end()) {
+		if (c_map.find(s[i]) != c_map.end()) {
 			pos.emplace_back(make_pair(i, s[i]));
 		}
 	}
@@ -32,8 +46,9 @@ string smallest_substr(string& s, vector<char>& c) {
 			count[pos[i].second]++;
 			count[pos[j].second]++;
 		}
-		
-		if (count.size() == c.size()) {
+
+		if (is_satisfied(c_map, count)) {
+			// The substring contains all characters.
 			int cur_len = pos[j].first - pos[i].first + 1;
 			string cur_str = s.substr(pos[i].first, cur_len);
 			if (cur_len < length || (cur_len == length && cur_str.compare(ret) < 0)) {
@@ -55,8 +70,10 @@ string smallest_substr(string& s, vector<char>& c) {
 }
 
 int main() {
-	string s = "qzdxbycaxtzabyk";
-	vector<char> c{'x', 'y', 'z'};
+	//	string s = "qzdxbycaxtzabyk";
+	//	vector<char> c{'x', 'y', 'z'};
+	string s = "caae";
+	vector<char> c{'c', 'a', 'e'};
 	string ret = smallest_substr(s, c);
 	cout << "Smallest substring of \"" << s << "\" is: " << ret << endl;
 	return 0;
