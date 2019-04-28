9067f99484843299ab3b93b3a00e1d0d2ef87ca5
Son Dinh <technology789@gmail.com>
2019-04-27 21:38:55 -0500

    add a solution using sort

--- others/128_LongestConsecutiveSequence.cpp
+++ others/128_LongestConsecutiveSequence.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    int longestConsecutive2(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        
+        sort(nums.begin(), nums.end());
+        int max = 0, count = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == nums[i-1])
+                continue;
+            if (nums[i] == nums[i-1]+1) {
+                ++count;
+            } else {
+                if (count > max) {
+                    max = count;
+                }
+                count = 1;
+            }
+        }
+        if (count > max)
+            max = count;
+        
+        return max;
+    }
+	
     int longestConsecutive(vector<int>& nums) {
         unordered_set<int> my_set(nums.begin(), nums.end());
         
