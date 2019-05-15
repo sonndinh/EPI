9619b35f0117af847951eea203c7bda54a871dd0
Son Dinh <technology789@gmail.com>
2019-05-15 16:21:35 -0500

    maximum concurrent events

---
+++ 14_5_RenderCalendar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct Interval {
+	int begin, end;
+	Interval(int b, int e) : begin(b), end(e) {}
+};
+
+// Return the maximum number of concurrent events.
+int max_concurrent_events(const vector<Interval>& events) {
+	
+}
+
+int main() {
+	return 0;
+}
