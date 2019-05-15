b20e4dd875f6c0261df1ea00fde8a72aecfcb35c
Son Dinh <technology789@gmail.com>
2019-05-15 17:05:05 -0500

    sorting solution

--- 14_5_RenderCalendar.cpp
+++ 14_5_RenderCalendar.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 struct Interval {
@@ -8,9 +10,35 @@ struct Interval {
 
 // Return the maximum number of concurrent events.
 int max_concurrent_events(const vector<Interval>& events) {
+	int n = events.size();
+	vector<int> begins(n), ends(n);
+	for (int i = 0; i < n; ++i) {
+		begins[i] = events[i].begin;
+		ends[i] = events[i].end;
+	}
 	
+	// Sort the intervals by their start times and end times.
+	sort(begins.begin(), begins.end());
+	sort(ends.begin(), ends.end());
+
+	int i = 0, j = 0;
+	int ret = 0;
+	for (int i = 0, j = 0; i < n && j < n; ) {
+		if (begins[i] <= ends[j]) {
+			++ret;
+			++i;
+		} else {
+			--ret;
+			++j;
+		}
+	}
+
+	return ret;
 }
 
 int main() {
+	vector<Interval> events{{1, 5}, {6, 10}, {11, 13}, {14, 15}, {2, 7}, {8, 9}, {12, 15}, {4, 5}, {9, 17}};
+	int ret = max_concurrent_events(events);
+	cout << "Max concurrent events: " << ret << endl;
 	return 0;
 }
