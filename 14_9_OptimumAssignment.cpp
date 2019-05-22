34347c9e5c67f7bcbb497bab56c40782e32cf6b2
Son Dinh <technology789@gmail.com>
2019-05-21 23:49:24 -0500

    optimal assignment

--- 14_9_OptimumAssignment.cpp
+++ 14_9_OptimumAssignment.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
 using namespace std;
 
-
+// Return set of pairs of tasks whose maximum sum is minimized.
+// Assuming there are even number of tasks.
+vector<pair<int, int>> opt_assign(const vector<int>& tasks) {
+	vector<pair<int, int>> ret;
+	sort(tasks.begin(), tasks.end());
+	
+	for (int i = 0, j = tasks.size(); i < j; ++i, --j) {
+		ret.emplace_back(tasks[i], tasks[j]);
+	}
+	
+	return ret;
+}
 
 int main() {
+	vector<int> tasks{2,3,4,5,6,7};
+	auto ret = opt_assign(tasks);
+	for (auto& p : ret) {
+		cout << "(" << p.first << ", " << p.second << ")" << endl;
+	}
+	
 	return 0;
 }
