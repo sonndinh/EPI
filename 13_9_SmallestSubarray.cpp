1c9a1de2e2ecbaba65d14bceacb8c967dc6dda29
Son Dinh <technology789@gmail.com>
2019-05-31 23:09:07 -0500

    smallest subarray that covers all keywords

---
+++ 13_9_SmallestSubarray.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+// Return the starting and ending indexes of a smallest subarray that covers the keywords.
+pair<int, int> smallest_subarray(const vector<string>& A, const unordered_set<string>& keys) {
+	
+}
+
+int main() {
+	return 0;
+}
