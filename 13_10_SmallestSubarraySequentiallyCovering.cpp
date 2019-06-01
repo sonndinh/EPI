173b0cd3a882a8a4e77617f4a2a1d24b3008c7d1
Son Dinh <technology789@gmail.com>
2019-06-01 16:44:28 -0500

    smallest subarray that sequentially covers keywords

---
+++ 13_10_SmallestSubarraySequentiallyCovering.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+using namespace std;
+
+// Find the smallest subarray of @text that covers all words in @keywords.
+// Words in @keywords are distinct.
+pair<int, int> smallest_subarray_sequential(const vector<string>& text, const vector<string>& keywords) {
+	if (text.empty() || keywords.empty()) return {-1, -1};
+
+	int start = 0, end = 0;
+	
+	return {start, end};
+}
+
+int main() {
+	return 0;
+}
