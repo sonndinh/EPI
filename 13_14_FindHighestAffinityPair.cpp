28238349d3dab771ed24a74ed1bab496f605fb73
Son Dinh <technology789@gmail.com>
2019-06-02 23:07:05 -0500

    function prototype

---
+++ 13_14_FindHighestAffinityPair.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <string>
+using namespace std;
+
+// Return a pair of pages with highest affinity.
+// Each pair in the input is a pair of page and user that views that page.
+pair<string, string> pair_with_highest_affinity(const vector<pair<string, string>>& lines) {
+	
+}
+
+int main() {
+	
+	return 0;
+}
