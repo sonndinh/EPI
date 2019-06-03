59c2b305b56ea4b5b6074df447542e8ad1a11253
Son Dinh <technology789@gmail.com>
2019-06-03 17:15:52 -0500

    prototype

---
+++ 13_18_FindShorestUniquePrefix.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+// Return the shortest prefix of a string which is not a prefix of any string in a dictionary.
+string shortest_prefix(const string& word, const unordered_set<string>& dict) {
+	
+}
+
+int main() {
+	return 0;
+}
